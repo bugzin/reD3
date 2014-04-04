d45badb54593aa95ffc942370a552a023d0b0a47
Gaurav Sharma <gks2480@gmail.com>
2014-04-04 17:46:42 +0530

    modified time_format

--- src/dayhourheatmap.js
+++ src/dayhourheatmap.js
@@ -16,10 +16,11 @@
                 height = options.height || 500,
                 colors = options.colors || ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"],
                 days = options.days || ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+                time_format = this.options.time_format || '',
                 oMargin = options.margin;
-            	
+
 			//24 Hours format option	
-			if(this.options.time_format = '24'){
+			if(time_format === '24'){
 				times = options.times || ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"];
 			} else {
 				times = options.times || ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
