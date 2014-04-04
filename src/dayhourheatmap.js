bb58d7379d7b9e3629a2445ab2a664ba7fbc4b4f
Laurent CORROCHANO <laurent@adages.net>
2014-04-04 12:22:37 +0200

    Update dayhourheatmap.js
    Add option time format and click event on rect elements

--- src/dayhourheatmap.js
+++ src/dayhourheatmap.js
@@ -16,9 +16,15 @@
                 height = options.height || 500,
                 colors = options.colors || ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"],
                 days = options.days || ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-                times = options.times || ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"],
                 oMargin = options.margin;
-
+            	
+			//24 Hours format option	
+			if(this.options.time_format = '24'){
+				times = options.times || ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"];
+			} else {
+				times = options.times || ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+			}
+            
             this.days = days;
             this.times = times;
             this.colors = colors;
@@ -114,7 +120,8 @@
                 .attr("class", "hour bordered")
                 .attr("width", gridSize)
                 .attr("height", gridSize)
-                .style("fill", colors[0]);
+                .style("fill", colors[0])
+                .on('click',this.options.onClick);
 
             heatMap.transition().duration(1000)
                 .style("fill", function(d) {
